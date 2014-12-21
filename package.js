7f6c71f9f17b3c4f525d20b0a7acd376029e102e
Kondratiev, Vitaly (UK - London) <vkondratiev@deloitte.co.uk>
2014-12-21 13:46:12 +0000

    initial commit

---
+++ package.js
@@ -0,0 +1,26 @@
+Package.describe({
+  name: 'vitkon:todoist',
+  summary: 'Todoist API package for meteor server',
+  version: '0.1.0',
+  git: 'https://github.com/vitkon/meteor-todoist.git'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0');
+  api.use([
+  	'underscore',
+    'mongo-livedata',
+    'http'], ['server']);
+  api.addFiles([
+    'vitkon:todoist.js',
+  ], ['server']);
+
+  if (api.export) {
+    api.export('Todoist');
+  }
+});
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('vitkon:todoist');
+  api.addFiles('vitkon:todoist-tests.js');
+});
---
+++ vitkon:todoist.js
@@ -0,0 +1,77 @@
+TodoistData = new Meteor.Collection("TodoistData");
+
+var baseUrl = 'https://todoist.com/API/';
+
+Todoist = function(config) {
+    var self = this;
+
+    if (!config || !config.email || !config.password) {
+        throw 'Must instantiate Todoist with an email and password';
+    }
+
+    config.baseUrl = config.baseUrl + 'https://todoist.com/API/';
+
+    this.user = TodoistData.findOne({
+        email: config.email
+    }) || {};
+
+    // if it's a new user
+    if (_.isEmpty(this.user)) {
+        var result,
+            userData;
+
+        try {
+            userData = this._get('login', {
+                email: config.email,
+                password: config.password
+            });
+
+            if (userData.data === 'LOGIN_ERROR') {
+                throw 'Incorrect username or password';
+            }
+
+            if (userData.data) {
+                this.user = userData.data;
+                TodoistData.remove({
+                    id: userData.data.id,
+                    email: userData.data.email
+                });
+                TodoistData.insert(userData.data);
+            }
+
+        } catch (err) {
+            console.log(err);
+        }
+
+    }
+
+    return this;
+};
+
+Todoist.prototype.request = function (endpoint, params) {
+    params = params || {};
+    endpoint = endpoint.toLowerCase();
+
+    if (!params.hasOwnProperty('token')) {
+        params.token = this.user.token;
+    }
+
+    return this._get(endpoint, params).data;
+};
+
+Todoist.prototype._get = function(endpoint, params) {
+    var path = config.baseUrl + endpoint,
+        response;
+
+    try {
+        response = HTTP.get(path, {
+            params: params || {}
+        });
+        return response;
+
+    } catch (err) {
+        return {
+            error: err
+        };
+    }
+};
